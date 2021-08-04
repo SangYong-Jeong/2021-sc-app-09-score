cba8b10736fdd2e8c9ce52c346aed913096e8711
SangYong-Jeong <mmaf@naver.com>
2021-08-04 15:04:48 +0900

    login

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,12 @@
+/* 
+false 판정: 0, null, undefined, '' , false
+true 판정: false 판정 외 모든 것
+- 0을 제외한 모든 숫자
+- ''(빈문자열)을 제외한 모든 문자
+- 객체, 함수 .... 전부 다
+*/
+
+
 /**************Global init ****************/
 console.log(firebase);
 var auth = firebase.auth();
@@ -8,15 +17,34 @@ var googleAuth = new firebase.auth.GoogleAuthProvider();
 
 
 /**************Event callback ****************/
-function onAuthChanged() {
-
+function onAuthChanged(user) { // auth 상태가 변하면 알려줘
+	if(user) {
+		console.log(user);
+		$('.bt-login').hide();
+		$('.bt-logout').show();
+		$('.photo-logo img').attr('src', user.photoURL);
+		$('.photo-logo').show();
+		$('.icon-logo').hide();
+	}
+	else {
+		$('.bt-login').show();
+		$('.bt-logout').hide();
+		$('.photo-logo').hide();
+		$('.icon-logo').show();
+	}
 }
 
 function onLogin() {
 	auth.signInWithPopup(googleAuth);
 }
 
+function onLogout() {
+	auth.signOut();
+}
+
 /**************Event init ****************/
-auth.onAuthStateChanged(onAuthChanged);
+auth.onAuthStateChanged(onAuthChanged); // auth 상태가 변하면 알려줘
 $('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
+
 
