3eec9c11581d92a0c5e2b02d2f0353d67af48807
SangYong-Jeong <mmaf@naver.com>
2021-08-09 10:32:51 +0900

    1교시 커밋

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,47 @@
-/* 
-false 판정: 0, null, undefined, '' , false
-true 판정: false 판정 외 모든 것
-- 0을 제외한 모든 숫자
-- ''(빈문자열)을 제외한 모든 문자
-- 객체, 함수 .... 전부 다
-*/
-
-
-/**************Global init ****************/
+/************** global init ***************/
 console.log(firebase);
 var auth = firebase.auth();
-var db = firebase.database();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
+var db = firebase.database();
+var user = null;
 
-/**************Function init ****************/
+/************** function init ***************/
 
 
-/**************Event callback ****************/
-function onAuthChanged(user) { // auth 상태가 변하면 알려줘
+/************** event callback ***************/
+function onAdded(v) {
+	var n = $('.test-wrapper .tbody tr').length + 1;
+	var html = '<tr>';
+	html += '<td>'+n+'</td>';
+	html += '<td>'+v.val().username+'</td>';
+	html += '<td class="text-left">'+v.val().comment+'</td>';
+	html += '<td>'+moment(v.val().createdAt).format('YYYY-MM-DD HH:mm:ss')+'</td>';
+	html += '</tr>';
+	$(html).prependTo('.test-wrapper .tbody');
+}
+
+function onSubmit(f) {
+	if(!user) alert('로그인 후 사용해 주세요.');
+	else {
+		var data = {
+			username: f.username.value.trim(),
+			comment: f.comment.value.trim(),
+			createdAt: new Date().getTime(),
+			uid: user.uid,
+			email: user.email
+		}
+		if(data.username !== '' && data.comment !== '') {
+			db.ref('/root/test').push(data);
+			f.reset();
+		}
+	}
+	return false;
+}
+
+function onAuthChanged(v) { // auth상태가 변하면 알려줘
+	user = v;
 	if(user) {
-		console.log(user);
+		db.ref('root/test').on('child_added', onAdded);
 		$('.bt-login').hide();
 		$('.bt-logout').show();
 		$('.photo-logo img').attr('src', user.photoURL);
@@ -32,19 +54,20 @@ function onAuthChanged(user) { // auth 상태가 변하면 알려줘
 		$('.photo-logo').hide();
 		$('.icon-logo').show();
 	}
+	$('.test-wrapper .tbody').empty();
 }
 
 function onLogin() {
 	auth.signInWithPopup(googleAuth);
+	// auth.signInWithRedirect(googleAuth);
 }
 
 function onLogout() {
 	auth.signOut();
 }
 
-/**************Event init ****************/
-auth.onAuthStateChanged(onAuthChanged); // auth 상태가 변하면 알려줘
+/************** event init ***************/
+auth.onAuthStateChanged(onAuthChanged); // auth상태가 변하면 알려줘
 $('.bt-login').click(onLogin);
 $('.bt-logout').click(onLogout);
-
-
+console.log(db.ref('/root/test'));
