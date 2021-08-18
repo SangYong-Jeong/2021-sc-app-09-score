30dd2e970e3ca470075c077540044f7539167106
SangYong-Jeong <mmaf@naver.com>
2021-08-18 16:20:16 +0900

    수정완료

--- public/js/08.file.js
+++ public/js/08.file.js
@@ -1,14 +1,51 @@
-/************* Global init ***************/
+/*************** global init **************/
+var auth = firebase.auth();
+var database = firebase.database();
+var storage = firebase.storage();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+var dbRoot = database.ref('root');
+var stRoot = storage.ref().child('imgs');
 
 
-/************* user function *************/
+/************** user function *************/
+function genFile() {
+	return new Date().getTime() + '_' + random(1000, 1000);
+}
 
+/************** event callback ************/
+function onAuthChanged(r) {
+	if(r) {
+		$('.bt-login').hide();
+		$('.bt-logout').show();
+	}
+	else {
+		$('.bt-login').show();
+		$('.bt-logout').hide();
+	}
+}
 
-/************* event callback ************/
+function onLogin() {
+	auth.signInWithPopup(googleAuth);
+}
 
+function onLogout() {
+	auth.signOut();
+}
 
-/************* event init ****************/
+function onSubmit(e) {
+	e.preventDefault();
+	var el = document.querySelector('input[name="upfile"]');
+	if(el.files.length) {
+		var file = document.querySelector('input[name="upfile"]').files[0]; // input type="file"
+		var savename = genFile();
+		var uploader = stRoot.child(savename).put(file);
+	}
+}
 
+/*************** event init ***************/
+auth.onAuthStateChanged(onAuthChanged);
+$('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
+$('form[name="uploadForm"]').submit(onSubmit);
 
-/************* start init ****************/
-
+/*************** start init ***************/
