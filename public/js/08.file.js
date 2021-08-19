12fcc54ccd03e83bc586170e9ca1f7b76f2fb991
SangYong-Jeong <mmaf@naver.com>
2021-08-19 10:27:22 +0900

    1교시 완료

--- public/js/08.file.js
+++ public/js/08.file.js
@@ -5,11 +5,15 @@ var storage = firebase.storage();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
 var dbRoot = database.ref('root');
 var stRoot = storage.ref().child('imgs');
-
+var upfile;
 
 /************** user function *************/
 function genFile() {
-	return new Date().getTime() + '_' + random(1000, 1000);
+	var folder = moment().format('YYYYMMDDHH');
+	return {
+		folder: folder,
+		file:  folder + '_' + uuidv4()
+	}
 }
 
 /************** event callback ************/
@@ -38,8 +42,37 @@ function onSubmit(e) {
 	if(el.files.length) {
 		var file = document.querySelector('input[name="upfile"]').files[0]; // input type="file"
 		var savename = genFile();
-		var uploader = stRoot.child(savename).put(file);
+		var uploader = stRoot.child(savename.folder).child(savename.file).put(file);
+		// console.log(uploader); -> 객체를 리턴해줌 이 객체는 업로드 하는 file을 의미 
+		uploader.on('state_changed', onUploading, onUploadError, onUploaded); // -> uploader의 state_changed는 callback 3개
+		// 이벤트 대상은 파일객체 -> 파일의 상태변화는 3가지 1) 업로딩중 2) 업로딩 완료 3) 업로드 에러 
 	}
+}
+
+function onUploading (snapshot) { // 현재 상태를 인자로 보내줌
+	console.log('uploading', snapshot.bytesTransferred);
+	console.log('uploading', snapshot.totalBytes);
+	console.log('================');
+	upfile = snapshot;
+}
+
+function onUploaded () {
+	upfile.ref.getDownloadURL().then(onSuccess).catch(onError); // ref는 file이 서버에 저장된 위치를 의미 거기서 downladeURL정보를 받아옴
+}
+
+function onUploadError (err) {
+	console.log('error', err);
+	if(err.code === 'storage/unauthorized') location.href = '../403.html'
+	else console.log('error', err);
+	// location.href = '../403.html' // -> 서버에 다시 요청하는것 err 가 뜨면 이 파일 보여줘 하는것
+}
+
+function onSuccess (r) { // 성공시 download URL을 인자로 받음 + 다양한 것들 존재
+	console.log(r);
+}
+
+function onError () {
+
 }
 
 /*************** event init ***************/
