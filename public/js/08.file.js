cb63302c81aaaeed4edc8fe870b5e1b0ae19ee8f
SangYong-Jeong <mmaf@naver.com>
2021-08-19 11:31:16 +0900

    2교시 완료

--- public/js/08.file.js
+++ public/js/08.file.js
@@ -3,9 +3,11 @@ var auth = firebase.auth();
 var database = firebase.database();
 var storage = firebase.storage();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
-var dbRoot = database.ref('root');
+var dbRoot = database.ref('root/uploads');
 var stRoot = storage.ref().child('imgs');
-var upfile;
+var user = null;
+var allowExt = ['jpg', 'jpeg', 'png', 'gif', 'mp4'];
+
 
 /************** user function *************/
 function genFile() {
@@ -18,7 +20,8 @@ function genFile() {
 
 /************** event callback ************/
 function onAuthChanged(r) {
-	if(r) {
+	user = r;
+	if(user) {
 		$('.bt-login').hide();
 		$('.bt-logout').show();
 	}
@@ -39,41 +42,66 @@ function onLogout() {
 function onSubmit(e) {
 	e.preventDefault();
 	var el = document.querySelector('input[name="upfile"]');
-	if(el.files.length) {
+	if(el.files.length && user) {
 		var file = document.querySelector('input[name="upfile"]').files[0]; // input type="file"
-		var savename = genFile();
-		var uploader = stRoot.child(savename.folder).child(savename.file).put(file);
-		// console.log(uploader); -> 객체를 리턴해줌 이 객체는 업로드 하는 file을 의미 
-		uploader.on('state_changed', onUploading, onUploadError, onUploaded); // -> uploader의 state_changed는 callback 3개
-		// 이벤트 대상은 파일객체 -> 파일의 상태변화는 3가지 1) 업로딩중 2) 업로딩 완료 3) 업로드 에러 
+		if(allowExt.indexOf(file.name.split('.').pop().toLowerCase()) > -1) {
+			var savename = genFile();
+			var uploader = stRoot.child(savename.folder).child(savename.file).put(file);
+			// console.log(uploader); -> 객체를 리턴해줌 이 객체는 업로드 하는 file을 의미 
+			uploader.on('state_changed', onUploading, onUploadError, onUploaded); // -> uploader의 state_changed는 callback 3개
+			// 이벤트 대상은 파일객체 -> 파일의 상태변화는 3가지 1) 업로딩중 2) 업로딩 완료 3) 업로드 에러 
+		}
+		else alert('업로드 가능한 파일은 이미지 또는 mp4영상입니다.')
+	}
+	else if (user === null) {
+		alert('로그인 후 시도해 주세요.');
+	}
+	else {
+		$('input[name="upfile"]').focus();
+	}
+
+	function onUploading (snapshot) { // 현재 상태를 인자로 보내줌
+		console.log('uploading', snapshot.bytesTransferred);
+		console.log('uploading', snapshot.totalBytes);
+		console.log('================');
+		upfile = snapshot;
+	}
+	
+	function onUploaded () {
+		upfile.ref.getDownloadURL().then(onSuccess).catch(onError); // ref는 file이 서버에 저장된 위치를 의미 거기서 downladeURL정보를 받아옴
+	}
+	
+	function onUploadError (err) {
+		console.log('error', err);
+		if(err.code === 'storage/unauthorized') location.href = '../403.html'
+		else console.log('error', err);
+		// location.href = '../403.html' // -> 서버에 다시 요청하는것 err 가 뜨면 이 파일 보여줘 하는것
+	}
+	
+	function onSuccess (r) { // 성공시 download URL을 인자로 받음 + 다양한 것들 존재
+		if(file.type.split('/')[0] === 'image') {
+			$('.main-img').attr('src', r).show();
+			$('.main-video').hide();
+		}
+		else if(file.type.split('/')[0] === 'video') {
+			$('.main-video').attr('src', r).show();
+			$('.main-img').hide();
+		}
+		var saveData = {
+			oriname: file.name,
+			savename: savename.file,
+			path: 'imgs/'+savename.folder,
+			type: file.type,
+			size: file.size,
+		}
+		console.log(file);
+		dbRoot.push(saveData);
+	}
+	function onError (err) {
+		console.log(err);
 	}
 }
 
-function onUploading (snapshot) { // 현재 상태를 인자로 보내줌
-	console.log('uploading', snapshot.bytesTransferred);
-	console.log('uploading', snapshot.totalBytes);
-	console.log('================');
-	upfile = snapshot;
-}
-
-function onUploaded () {
-	upfile.ref.getDownloadURL().then(onSuccess).catch(onError); // ref는 file이 서버에 저장된 위치를 의미 거기서 downladeURL정보를 받아옴
-}
-
-function onUploadError (err) {
-	console.log('error', err);
-	if(err.code === 'storage/unauthorized') location.href = '../403.html'
-	else console.log('error', err);
-	// location.href = '../403.html' // -> 서버에 다시 요청하는것 err 가 뜨면 이 파일 보여줘 하는것
-}
-
-function onSuccess (r) { // 성공시 download URL을 인자로 받음 + 다양한 것들 존재
-	console.log(r);
-}
-
-function onError () {
-
-}
 
 /*************** event init ***************/
 auth.onAuthStateChanged(onAuthChanged);
--- repeat2.md
+++ repeat2.md
@@ -1,1 +1,9 @@
-# uuid(범용 식별자)
+# uuid(범용 식별자) -> uuid.js 파일을 이용해서 uuid v4 생성 가능
+
+# onUploading, onUploadError, onUploaded, onSuccess, onError -> onsubmit 안에 이 함수들을 넣은 이유는 외부에서 쓰이지 않고 onSubmit에서만 쓰이는 변수들을 같이 쓰기 위함이다.
+
+# video는 태그 달고 그안에 source를 만들어서 사용할 수 있다. 아니면 video 태그만으로도 가능 -> 웹에서 mp4는 다 돌아가는데 다른 확장자명은 안 돌아갈 수 있음. 
+## video controls : 바가 생기고 시간 이동가능
+## video autoplay : 자동재생 (muted 있을 때에만 가능)
+## video muted : 음소거
+## video loop : 무한반복
