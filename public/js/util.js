53e245e7d89aab0c5887780dc42fab4dc112e42a
SangYong-Jeong <mmaf@naver.com>
2021-08-20 16:26:57 +0900

    수업중

--- public/js/util.js
+++ public/js/util.js
@@ -10,3 +10,22 @@ function zp(n) {
 	// else return n;
 	return (n < 10) ? '0'+n : n;
 }
+
+// 현 시점의 시간으로 폴더명과 파일명을 생서 - uuid 참조 필요.
+function genFile() {
+	var folder = moment().format('YYYYMMDDHH');
+	return {
+		folder: folder,
+		file:  folder + '_' + uuidv4()
+	}
+}
+
+// 동영상의 초를 시:분:초로 리턴하는 함수
+function getPlayTime (s) {
+	if(Math.floor(s/60) >= 60) {
+		return zp(Math.floor(s/60/60)) + ':' + zp(Math.floor(s/60%60)) +':'+ zp(s%60);
+	}
+	else {
+		return zp(Math.floor(s/60))+':'+zp(s%60);
+	}
+}
